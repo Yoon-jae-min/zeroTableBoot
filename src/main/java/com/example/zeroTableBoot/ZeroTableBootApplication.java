df643e5ee18642186fb78cf31827a22be9f14309
Yoon-Jae-Min <jmyoon1994@naver.com>
2024-04-13 10:54:07 +0900

    20240413-home

---
+++ src/main/java/com/example/zeroTableBoot/ZeroTableBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.zeroTableBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroTableBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroTableBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zeroTableBoot/ZeroTableBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zeroTableBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroTableBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
